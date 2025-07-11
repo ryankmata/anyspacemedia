5469b07d9c38dc76b332c21fe23878dba0ab50a5
ryankmata <ryankmata@gmail.com>
2025-07-11 10:55:02 -0600

    Setup intial file structure

---
+++ src/app/architecture-design/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+    return <p>Hello Architecture & Design</p>
+  }
+  
---
+++ src/app/real-estate/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+    return <p>Hello Real Estate</p>
+  }
+  
