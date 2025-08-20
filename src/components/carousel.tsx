172c5241354efd2f63e39082784ab56202b962b2
ryankmata <ryankmata@gmail.com>
2025-08-20 16:56:33 -0600

    update global css

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,7 +1,6 @@
 "use client";
 import dynamic from "next/dynamic";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+
 import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
